7d0a4c89be4dfc20f1ad4e4b64537dbba6fd20e2
Ning Yu Luo <sslnyx@gmail.com>
2022-08-13 01:19:32 -0700

    player selector update

--- src/components/PlayersModalContent.jsx
+++ src/components/PlayersModalContent.jsx
@@ -1,28 +1,70 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useOutletContext } from "react-router-dom";
 
 const PlayersModalContent = ({ players, setPlayers }) => {
   const { allPlayers } = useOutletContext();
   const [error, setError] = useState(false);
-  const [selectedPlayer, setSelectedPlayer] = useState(null);
+  const [selectedPlayer, setSelectedPlayer] = useState("p1");
   const activePlayersRef = useRef({});
+  const allPlayersRef = useRef({});
 
   const removePlayer = (inputId, ev) => {
     Object.values(activePlayersRef.current).forEach((playerBtn) => {
-      playerBtn.classList.remove("!bg-red-500");
+      playerBtn.classList.remove("!bg-red-500", "scale-[1.2]");
     });
-    ev.target.classList.add("!bg-red-500");
+    ev.target.classList.add("!bg-red-500", "scale-[1.2]");
     players.find(({ id }) => id === inputId).name = inputId.toUpperCase();
     setPlayers([...players]);
     setSelectedPlayer(inputId);
   };
 
-  const playerHandler = (player) => {
+  const playerHandler = (player, ev) => {
     selectedPlayer
       ? ((players.find(({ id }) => selectedPlayer === id).name = player),
         setPlayers([...players]))
       : setError(true);
+
+    const thePlayer = players.find(({ id }) => selectedPlayer === id);
+
+    const nextPlayer =
+      players.indexOf(thePlayer) + 1 > 3
+        ? "p1"
+        : players[players.indexOf(thePlayer) + 1].id;
+
+    Object.values(activePlayersRef.current).forEach((playerBtn) => {
+      playerBtn.classList.remove("!bg-red-500", "scale-[1.2]");
+    });
+
+    activePlayersRef.current[nextPlayer].classList.add(
+      "!bg-red-500",
+      "scale-[1.2]"
+    );
+
+    setSelectedPlayer(nextPlayer);
   };
+
+  useEffect(() => {
+    activePlayersRef.current[selectedPlayer].classList.add(
+      "!bg-red-500",
+      "scale-[1.2]"
+    );
+  }, []);
+
+  useEffect(() => {
+    Object.entries(allPlayersRef.current).forEach(([name, playerBtn]) => {
+      playerBtn.classList.remove("!bg-red-500");
+    });
+
+    Object.entries(allPlayersRef.current).forEach(([name, playerBtn]) => {
+      if (
+        Object.values(activePlayersRef.current).find(
+          (el) => el.innerHTML === name
+        )
+      ) {
+        playerBtn.classList.add("!bg-red-500");
+      }
+    });
+  }, [selectedPlayer]);
 
   return (
     <div className="text-center">
@@ -30,9 +72,12 @@ const PlayersModalContent = ({ players, setPlayers }) => {
         <h2 className="mb-3">邊個玩緊?</h2>
         <div className="flex justify-center flex-wrap">
           {players.map(({ name, id }) => (
-            <button key={id} className="p-1 text-white last:mr-0 basis-1/2">
+            <button
+              key={id}
+              className="px-3 py-2 text-white last:mr-0 basis-1/2"
+            >
               <div
-                className="py-2 px-5 bg-sky-500 rounded"
+                className="transition-all duration-500 ease-out py-2 px-5 bg-sky-500 rounded"
                 ref={(el) => (activePlayersRef.current[id] = el)}
                 onClick={(ev) => removePlayer(id, ev)}
               >
@@ -48,17 +93,12 @@ const PlayersModalContent = ({ players, setPlayers }) => {
       <div className="border rounded p-3">
         <h2 className="mb-3">揀人啦！</h2>
 
-        {error && !selectedPlayer ? (
-          <p className="mb-3 text-red-500">揀個玩緊既人先啦</p>
-        ) : (
-          ""
-        )}
-
         <div className="flex flex-wrap justify-center">
           {allPlayers.map((player, i) => (
             <button
               key={i}
-              onClick={() => playerHandler(player)}
+              ref={(el) => (allPlayersRef.current[player] = el)}
+              onClick={(ev) => playerHandler(player, ev)}
               className="py-2 px-5 bg-sky-500 mb-3 mr-3 rounded text-white"
             >
               {player}
